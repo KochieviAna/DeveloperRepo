4c8e015a79ae7ea2883860685bfa6cb3b6733f5f
KochieviAna <anna.kochievi@gmail.com>
2024-11-04 04:02:13 +0400

    Working on signin page

--- Assignment21/Assignment21/Presenters/LogInPage/LogInPageVC.swift
+++ Assignment21/Assignment21/Presenters/LogInPage/LogInPageVC.swift
@@ -30,6 +30,94 @@ class LogInPageVC: UIViewController {
         return imageView
     }()
     
+    private lazy var usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = .SenRegular(size: 16)
+        label.textColor = UIColor(hexString: "FFFFFF")
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter username"
+        textField.font = .SenMedium(size: 11)
+        textField.textColor = UIColor(hexString: "2B0063")
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = 5
+        textField.keyboardType = .default
+        textField.textContentType = .username
+        textField.backgroundColor = UIColor(hexString: "FFFFFF")
+        
+        return textField
+    }()
+    
+    private lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password"
+        label.font = .SenRegular(size: 16)
+        label.textColor = UIColor(hexString: "FFFFFF")
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter password"
+        textField.font = .SenMedium(size: 11)
+        textField.textColor = UIColor(hexString: "2B0063")
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = 5
+        textField.keyboardType = .default
+        textField.textContentType = .password
+        textField.backgroundColor = UIColor(hexString: "FFFFFF")
+        
+        return textField
+    }()
+    
+    private lazy var confirmPasswordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Confirm password"
+        label.font = .SenRegular(size: 16)
+        label.textColor = UIColor(hexString: "FFFFFF")
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private lazy var confirmPasswordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter password"
+        textField.font = .SenMedium(size: 11)
+        textField.textColor = UIColor(hexString: "2B0063")
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = 5
+        textField.keyboardType = .default
+        textField.textContentType = .password
+        textField.backgroundColor = UIColor(hexString: "FFFFFF")
+        
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(UIColor(hexString: "FFFFFF"), for: .normal)
+        button.backgroundColor = UIColor(hexString: "8E84FF")
+        button.layer.cornerRadius = 5
+        button.addAction(UIAction(handler: { [ weak self ] action in
+            self?.signinButtonTapped()
+        }), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -40,6 +128,12 @@ class LogInPageVC: UIViewController {
         
         view.addSubview(imageWrapperImage)
         imageWrapperImage.addSubview(userImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(usernameTextField)
+        view.addSubview(passwordLabel)
+        view.addSubview(passwordTextField)
+        view.addSubview(confirmPasswordLabel)
+        view.addSubview(confirmPasswordTextField)
         
         setupConstraints()
     }
@@ -60,6 +154,47 @@ class LogInPageVC: UIViewController {
             make.height.equalTo(53.45 * Constraint.yCoeff)
             make.width.equalTo(74.69 * Constraint.xCoeff)
         }
+        
+        usernameLabel.snp.remakeConstraints { make in
+            make.top.equalTo(imageWrapperImage.snp.bottom).offset(22.79 * Constraint.yCoeff)
+            make.leading.equalToSuperview().offset(26 * Constraint.xCoeff)
+            make.trailing.equalToSuperview().offset(-26 * Constraint.xCoeff)
+        }
+        
+        usernameTextField.snp.remakeConstraints { make in
+            make.top.equalTo(usernameLabel.snp.bottom).offset(8 * Constraint.yCoeff)
+            make.leading.equalTo(usernameLabel.snp.leading)
+            make.trailing.equalTo(usernameLabel.snp.trailing)
+        }
+        
+        passwordLabel.snp.remakeConstraints { make in
+            make.top.equalTo(usernameTextField.snp.bottom).offset(13 * Constraint.yCoeff)
+            make.leading.equalTo(usernameTextField.snp.leading)
+            make.trailing.equalTo(usernameTextField.snp.trailing)
+        }
+        
+        passwordTextField.snp.remakeConstraints { make in
+            make.top.equalTo(passwordLabel.snp.bottom).offset(8 * Constraint.yCoeff)
+            make.leading.equalTo(passwordLabel.snp.leading)
+            make.trailing.equalTo(passwordLabel.snp.trailing)
+        }
+        
+        confirmPasswordLabel.snp.remakeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(13 * Constraint.yCoeff)
+            make.leading.equalTo(passwordTextField.snp.leading)
+            make.trailing.equalTo(passwordTextField.snp.trailing)
+        }
+        
+        confirmPasswordTextField.snp.remakeConstraints { make in
+            make.top.equalTo(confirmPasswordLabel.snp.bottom).offset(8 * Constraint.yCoeff)
+            make.leading.equalTo(confirmPasswordLabel.snp.leading)
+            make.trailing.equalTo(confirmPasswordLabel.snp.trailing)
+        }
+    }
+    
+    private func signinButtonTapped() {
+        let vc = QuizPageVC()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
