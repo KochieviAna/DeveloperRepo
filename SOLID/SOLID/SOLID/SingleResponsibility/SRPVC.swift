07ab739adfa0a438c7a94337bc3c09eb782eafd6
KochieviAna <anna.kochievi@gmail.com>
2024-11-13 18:00:16 +0400

    SRP and Not SRP examples added

--- SOLID/SOLID/SOLID/SingleResponsibility/SRPVC.swift
+++ SOLID/SOLID/SOLID/SingleResponsibility/SRPVC.swift
@@ -7,3 +7,20 @@
 
 import Foundation
 
+// MARK: მოცემულ მაგალითში ცალ-ცალკე არის გამოყოფილი SalesOrder-ს კლასი, რომელიც პასუხისმგებელია პროდუქტის დამატებაზე და EmailService კლასი რომელიც აგზავნის დადასტურების მეილს. შესაბამისად თითოეული კლასის ფუნქციონალთა რაოდენობა არის ერთის ტოლი და ემსახურება ერთ კონკრეტულ საქმეს, რაც აკმაყოფილებს SR პრინციპს.
+
+private final class SalesOrder {
+    var items: [String] = []
+    var total: Double = 0.0
+
+    func addItem(item: String, price: Double) {
+        items.append(item)
+        total += price
+    }
+}
+
+private final class EmailService {
+    func sendConfirmationEmail(for order: SalesOrder) {
+        print("Sending confirmation email for items: \(order.items) with total: \(order.total)")
+    }
+}
