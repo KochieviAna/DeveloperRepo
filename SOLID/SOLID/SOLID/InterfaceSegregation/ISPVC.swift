9d11c3f2281b5a825b78db02c189d4e027b88b37
KochieviAna <anna.kochievi@gmail.com>
2024-11-13 18:44:54 +0400

    ISP and NotISP examples added.

--- SOLID/SOLID/SOLID/InterfaceSegregation/ISPVC.swift
+++ SOLID/SOLID/SOLID/InterfaceSegregation/ISPVC.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+// MARK: მოცემულ მაგალითში შემუშავებულია ორი პროტოკოლი: SalesTask და Reporting, რაც ქვემოთ მოცემულ კლასებს საშუალებას აძლევს საჭიროებისამებრ დააკონფორმონ შესაბამისი პროტოკოლი/ები. ეს შემთხვევა შესაბამისობაში მოდის IS პრინციპთან.
+
+protocol SalesTask {
+    func makeSale()
+}
+
+protocol Reporting {
+    func generateMonthlyReport()
+}
+
+private final class PartTimeRepresentative: SalesTask {
+    func makeSale() {
+        print("Making a sale.")
+    }
+}
+
+final class FullTimeRepresentative: SalesTask, Reporting {
+    func makeSale() {
+        print("Making a sale.")
+    }
+    
+    func generateMonthlyReport() {
+        print("Generating monthly report.")
+    }
+}
