5291466efa04f5ea9823a4ff67b4fd71f9ecb4f0
KochieviAna <anna.kochievi@gmail.com>
2024-11-13 18:57:16 +0400

    DIP and NotDIP examples added.

--- SOLID/SOLID/SOLID/DependencyInversion/DIPVC.swift
+++ SOLID/SOLID/SOLID/DependencyInversion/DIPVC.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+// MARK: მოცემულ მაგალითში PaymentProcessor პროტოკოლი საშუალებას აძლევს კლასებს დამოკიდებული იყვნენ აბსტრაქციაზე და შეინარჩუნონ მოქნილობა ყველა ტიპის გადახდის მეოდის გამოყენებისას, რაც შესაბამისობაში მოდის DI პრინციპთან.
+
+protocol PaymentProcessor {
+    func processPayment(amount: Double)
+}
+
+private final class CreditCardProcessor: PaymentProcessor {
+    func processPayment(amount: Double) {
+        print("Processing credit card payment of \(amount)")
+    }
+}
+
+final class PayPalProcessor: PaymentProcessor {
+    func processPayment(amount: Double) {
+        print("Processing PayPal payment of \(amount)")
+    }
+}
+
+private final class SalesService {
+    let paymentProcessor: PaymentProcessor
+    
+    init(paymentProcessor: PaymentProcessor) {
+        self.paymentProcessor = paymentProcessor
+    }
+    
+    func completeSale(amount: Double) {
+        paymentProcessor.processPayment(amount: amount)
+    }
+}
--- SOLID/SOLID/SOLID/DependencyInversion/NotDIPVC.swift
+++ SOLID/SOLID/SOLID/DependencyInversion/NotDIPVC.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: მოცემულ მაგალითში SalesService კლასი არის დამოკიდებულლი CreditCardProcessor კლასზე რაც არღვევს DI პრინციპს მჭიდრო დამოკიდებულების შექმნით.
+
+private final class CreditCardProcessor {
+    func processPayment(amount: Double) {
+        print("Processing credit card payment of \(amount)")
+    }
+}
+
+private final class SalesService {
+    let paymentProcessor = CreditCardProcessor()
+    
+    func completeSale(amount: Double) {
+        paymentProcessor.processPayment(amount: amount)
+    }
+}
