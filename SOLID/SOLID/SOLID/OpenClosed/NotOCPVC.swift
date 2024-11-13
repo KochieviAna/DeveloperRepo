71e8e36746dad21a7c96461e63e7245790cb8429
KochieviAna <anna.kochievi@gmail.com>
2024-11-13 18:16:56 +0400

    OCP and NotOCP examples are added.

--- SOLID/SOLID/SOLID/OpenClosed/NotOCPVC.swift
+++ SOLID/SOLID/SOLID/OpenClosed/NotOCPVC.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+// MARK: DiscountCalculator კლასი მოიცავს ფუნქციას რომელიც უზრუნველყოფს ფასდაკების ორი ტიპის განსაზღვრას (percentage და fixed), შესაბამისად იძლევა შეზღუდულ შესაძლებლობას დამატებითი ტიპის განსაზღვრის შემთხვევაში, სწორედ ამიტომ DiscountCalculator კლასი ეწინაღმდეგება OC პრინციპს.
+
+private final class DiscountCalculator {
+    func calculateDiscount(amount: Double, discountType: String) -> Double {
+        if discountType == "percentage" {
+            return amount * 0.10
+        } else if discountType == "fixed" {
+            return amount - 10.0
+        } else {
+            return amount
+        }
+    }
+}
--- SOLID/SOLID/SOLID/OpenClosed/OCPVC.swift
+++ SOLID/SOLID/SOLID/OpenClosed/OCPVC.swift
@@ -7,3 +7,26 @@
 
 import Foundation
 
+// MARK: შემდგომ მაგალითში მოცემულია Discount პროტოკოლი, გააჩნია ფუნქციონალი applyDiscount, რომელიც უზრუნველყოფს ფასდაკლების დამატების განზოგადებას, რასაც კარგად ვხედავთ მომდევნო კლასებში იმპლემენტაციის დროს: PercentageDiscount-იყენებს Discount პროტოკოლს იმისთვის რომ გამოითვალოს კონკრეტული რაოდენობის  ფასდაკლება, FixedDiscount იყენებს ფიქსირებული ფასდაკლებისთვის, ხოლო DiscountCalculator ითვლის კონკრეტული თანხის რაოდენობაზე ფასდაკლების მნიშვნელობას. ყობელივე ზემოხსენებული შესაბამისობაში მოდის OC პრინციპთან.
+
+protocol Discount {
+    func applyDiscount(to amount: Double) -> Double
+}
+
+class PercentageDiscount: Discount {
+    func applyDiscount(to amount: Double) -> Double {
+        return amount * 0.10
+    }
+}
+
+class FixedDiscount: Discount {
+    func applyDiscount(to amount: Double) -> Double {
+        return amount - 10.0
+    }
+}
+
+private final class DiscountCalculator {
+    func calculateDiscount(amount: Double, discount: Discount) -> Double {
+        return discount.applyDiscount(to: amount)
+    }
+}
