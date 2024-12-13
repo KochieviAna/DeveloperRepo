566472180eaeed8d77e28f47ac2dcf216247b7f0
KochieviAna <anna.kochievi@gmail.com>
2024-12-13 17:02:18 +0100

    Task Completed.

--- Assignment30/Assignment30/Presenters/TimerDetailView/TimerDetailsView.swift
+++ Assignment30/Assignment30/Presenters/TimerDetailView/TimerDetailsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TimerDetailsView: View {
     @Binding var timer: TimerModel
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         ZStack {
@@ -16,84 +17,158 @@ struct TimerDetailsView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
-                titleText
-                    .padding(.top, 0)
-                    .padding(.bottom, 0)
+                HStack{
+                    arrowBackButton
+                    
+                    Spacer()
+                    
+                    titleText
+                    
+                    Spacer(minLength: 30)
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+                .frame(height: 110)
+                .background(Color.primaryDarkGrey)
                 
                 ScrollView {
                     VStack {
                         VStack {
-                            Image(systemName: "timer")
-                                .foregroundColor(.primaryOrange)
-                            Text(formatTime(timer.timeRemaining))
-                                .font(.interBold(size: 36))
-                                .foregroundStyle(.primaryBlue)
+                            timerImage
+                                .padding()
+                            
+                            durationText
+                                .padding()
+                            
+                            timeText
+                                .padding()
                         }
-                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 328)
+                        .background(Color.primaryDarkGrey)
+                        .cornerRadius(16)
+                        .padding(.top, 16)
+                        .padding(.horizontal)
                         
                         VStack(alignment: .leading) {
-                            Text("აქტივობის ისტორია")
-                                .font(.interMedium(size: 18))
+                            activityHistoryText
                                 .padding([.top, .horizontal])
-                                .foregroundStyle(.primaryWhite)
+                                .padding(16)
+                            
+                            dividerColor
+                                .padding(.horizontal)
                             
                             HStack {
-                                Text("თარიღი")
-                                    .font(.interMedium(size: 14))
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .foregroundStyle(.primaryWhite)
-                                Text("დრო")
-                                    .font(.interMedium(size: 14))
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundStyle(.primaryWhite)
+                                dateText
+                                
+                                remainingTimeText
+                                    .offset(x: 70)
                             }
-                            .padding(.horizontal)
-                            .padding(.bottom, 5)
+                            .padding(.horizontal, 28)
+                            .padding([.top, .bottom], 16)
                             
                             ForEach(timer.usageHistory, id: \.date) { history in
                                 HStack {
-                                    Text(formattedGeorgianDate(from: history.date))
-                                        .font(.interMedium(size: 14))
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                    
-                                    Text(formatTime(history.remainingTime))
-                                        .font(.interMedium(size: 14))
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
+                                    formattedDateText(for: history.date)
+                                    formattedTimeText(for: history.remainingTime)
+                                        .offset(x: 70)
                                 }
-                                .padding(.horizontal)
-                                Divider()
+                                .padding(.bottom)
+                                .padding(.horizontal, 28)
                             }
                         }
+                        .frame(maxWidth: .infinity)
+                        .background(Color.primaryDarkGrey)
+                        .cornerRadius(16)
+                        .padding(.horizontal)
                     }
-                    .padding(.top, 0)
                 }
             }
         }
         .navigationBarBackButtonHidden(true)
     }
     
-    private var titleText: some View {
-        LazyVStack {
-            Text(timer.title)
-                .font(.interBold(size: 24))
-                .foregroundStyle(.primaryWhite)
-                .padding(.top, 0)
-                .padding(.bottom, 0)
+    private var arrowBackButton: some View {
+        Button(action: {
+            dismiss()
+        }) {
+            Image("arrowBackButton")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.primaryWhite)
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .frame(height: 110)
-        .background(Color.primaryDarkGrey)
+        .frame(width: 24, height: 24)
     }
     
-    private func formatTime(_ time: Int) -> String {
-        let hours = time / 3600
-        let minutes = (time % 3600) / 60
-        let seconds = time % 60
-        
-        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    private var titleText: some View {
+        Text(timer.title)
+            .font(.interBold(size: 24))
+            .foregroundStyle(.primaryWhite)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .multilineTextAlignment(.center)
     }
+    
+    private var timerImage: some View {
+        Image("timerImage")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 70, height: 70)
+    }
+    
+    private var durationText: some View {
+        Text("ხანგრძლივობა")
+            .font(.interMedium(size: 18))
+            .foregroundStyle(.primaryWhite)
+    }
+    
+    private var timeText: some View {
+        Text(formatTime(timer.timeRemaining))
+            .font(.interBold(size: 36))
+            .foregroundStyle(.primaryBlue)
+    }
+    
+    private var activityHistoryText: some View {
+        Text("აქტივობის ისტორია")
+            .font(.interMedium(size: 18))
+            .foregroundStyle(.primaryWhite)
+    }
+    
+    private var dividerColor: some View {
+        Color.primaryWhite
+            .frame(maxWidth: .infinity, alignment: .center)
+            .frame(height: 1)
+    }
+    
+    private var dateText: some View {
+        Text("თარიღი")
+            .font(.interMedium(size: 14))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundStyle(.primaryWhite)
+    }
+    
+    private var remainingTimeText: some View {
+        Text("დრო")
+            .font(.interMedium(size: 14))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundStyle(.primaryWhite)
+    }
+    
+    private func formattedDateText(for date: Date) -> some View {
+        Text(formattedGeorgianDate(from: date))
+            .font(.interMedium(size: 14))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundStyle(.primaryWhite)
+    }
+    
+    private func formattedTimeText(for time: Int) -> some View {
+        Text(formatTime(time))
+            .font(.interMedium(size: 14))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundStyle(.primaryWhite)
+    }
+    
 }
 
 #Preview {
-    TimerDetailsView(timer: .constant(TimerModel(title: "Sample Timer", duration: 3600)))
+    TimerDetailsView(timer: .constant(TimerModel(title: "საცდელი ტაიმერი", duration: 3600)))
 }
---
+++ Assignment30/Assignment30/Resources/Helpers/FormatTime.swift
@@ -0,0 +1,16 @@
+//
+//  FormatTime.swift
+//  Assignment30
+//
+//  Created by MacBook on 13.12.24.
+//
+
+import SwiftUI
+
+func formatTime(_ time: Int) -> String {
+    let hours = time / 3600
+    let minutes = (time % 3600) / 60
+    let seconds = time % 60
+    
+    return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+}
