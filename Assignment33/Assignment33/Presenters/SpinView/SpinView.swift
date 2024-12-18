347cc78109b43dcc0a8fd235ef4e05c8e0fb1d18
KochieviAna <anna.kochievi@gmail.com>
2024-12-18 12:14:36 +0100

    SpinView added.

--- Assignment33/Assignment33/Presenters/SpinView/SpinView.swift
+++ Assignment33/Assignment33/Presenters/SpinView/SpinView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct SpinView: View {
+    @StateObject private var viewModel = SpinViewModel()
+    
     var body: some View {
         ZStack {
             backgroundColor
+            
+            Circle()
+                .strokeBorder(lineWidth: 4)
+                .frame(width: 300, height: 300)
+                .overlay(
+                    ForEach(0..<viewModel.sectionNames.count, id: \.self) { index in
+                        let angle = Angle(degrees: Double(index) * (360.0 / Double(viewModel.sectionNames.count)))
+                        WheelSection(
+                            sectionName: viewModel.sectionNames[index],
+                            startAngle: angle,
+                            totalSections: viewModel.sectionNames.count,
+                            color: viewModel.sectionColors[index % viewModel.sectionColors.count]
+                        )
+                    }
+                )
+                .rotationEffect(Angle(degrees: viewModel.rotationAngle))
+            
+            Circle()
+                .fill(Color.primaryWhite)
+                .frame(width: 30, height: 30)
+            
+            Triangle()
+                .fill(Color.red)
+                .frame(width: 40, height: 40)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -150)
+                .shadow(radius: 10)
         }
+        .gesture(
+            DragGesture()
+                .onEnded { value in
+                    if value.translation.height > 50 && !viewModel.isSpinning {
+                        viewModel.startSpin()
+                    }
+                }
+        )
     }
     
     private var backgroundColor: Color {
---
+++ Assignment33/Assignment33/Presenters/SpinView/SpinViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  SpinViewModel.swift
+//  Assignment33
+//
+//  Created by MacBook on 18.12.24.
+//
+
+import SwiftUI
+
+class SpinViewModel: ObservableObject {
+    @Published var rotationAngle: Double = 0.0
+    @Published var isSpinning = false
+    
+    let sectionNames = ["Gift Card", "Vacation", "iPhone", "House", "MacBook", "Car"]
+    let sectionColors = [Color.orange, Color.yellow, Color.green, Color.blue, Color.indigo, Color.purple]
+    
+    func startSpin() {
+        isSpinning = true
+        let totalSpin = Double.random(in: 720...1440)
+        withAnimation(.easeOut(duration: 3)) {
+            rotationAngle += totalSpin
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.isSpinning = false
+        }
+    }
+}
