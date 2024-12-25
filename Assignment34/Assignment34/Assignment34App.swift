bd11f45cc1841f7ccd2e06a6ff0ee043cf12d6d3
KochieviAna <anna.kochievi@gmail.com>
2024-12-25 15:26:34 +0100

    Assignment34 project added.

---
+++ Assignment34/Assignment34/Assignment34App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment34App.swift
+//  Assignment34
+//
+//  Created by MacBook on 25.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment34App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
